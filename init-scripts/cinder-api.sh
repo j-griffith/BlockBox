175c4a45a98eec395fbb7177c071698c24e6cb21
John Griffith <john.griffith8@gmail.com>
2017-05-10 08:15:58 -0600

    Add a restart to sched and cinderflex from Dims
    Epic keynote failure caused by startup race between Mariadb and
    c-scheduler.  Add a restart on-failure to c-scheduler to address
    this nightmare.
    
    Also, Dims whipped up a bash version of cinderflex.  Throw that in
    here too!!

--- init-scripts/cinder-api.sh
+++ init-scripts/cinder-api.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+INIT_DB=${INIT_DB:-true}
 
+if [ "$INIT_DB" = "true" ]; then
 /bin/sh -c "cinder-manage db sync" cinder
+fi
+
 mkdir -p /var/log/cinder
 touch /var/log/cinder/cinder-api.log
 cinder-api -d --log-file /var/log/cinder/cinder-api.log
